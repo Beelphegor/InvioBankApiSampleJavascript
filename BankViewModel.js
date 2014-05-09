20bf7cdb62f4d7524f1d68081d24f35bc7ab3e31
Byron Sommardahl <byron@acklenavenue.com>
2014-05-09 18:01:39 -0500

    Initial commit.

---
+++ BankViewModel.js
@@ -0,0 +1,99 @@
+﻿var BankViewModel = function (apiClient) {
+
+    var date = ko.observable(moment().format('YYYY-MM-DD'));
+    var payments = ko.observableArray();
+    var paymentsProcessed = ko.observableArray();
+    var remittances = ko.observableArray();
+    var remittancesProcessed = ko.observableArray();
+
+    var getRemittances = function () {
+        remittances.removeAll();
+        apiClient.Remittances.GetAll()
+            .then(function (response) {
+                $.each(response.remittances, function (index, r) {
+                    r = $.extend({}, r, {
+                        processing: ko.observable(false),
+                    });
+                    remittances.push(r);
+                });
+            });
+    };
+
+    getRemittances();
+
+    var getPayments = function (thisDate) {
+        payments.removeAll();
+        apiClient.Payments.GetAllForDate(thisDate)
+            .then(function (response) {
+                $.each(response.payments, function (index, p) {
+                    p = $.extend({}, p, {
+                        processing: ko.observable(false),
+                        paymentDateFormatted: moment(p.paymentDate).format('YYYY-MM-DD')
+                    });
+                    payments.push(p);
+                });
+            });
+    };
+
+    getPayments(date());
+
+    date.subscribe(function (newVal) {
+        getPayments(newVal);
+    });
+
+    var processPayment = function (payment) {
+        payment.processing(true);
+        var reference = prompt("Transaction Reference #");
+        var newBalance = prompt("New Account Balance");
+        apiClient.Payments.ProcessPayment(payment.id, newBalance, reference).then(function () {
+            payments.remove(payment);
+            payment = $.extend({}, payment, { status: 'Exito', reason: '', reference: reference });
+            paymentsProcessed.push(payment);
+        });
+    };
+
+    var failPayment = function (payment) {
+        payment.processing(true);
+        var reference = prompt("Transaction Reference #");
+        var reason = prompt("Reason for failure");
+        apiClient.Payments.FailPayment(payment.id, reason, reference).then(function () {
+            payments.remove(payment);
+            payment = $.extend({}, payment, { status: 'Error', reason: reason, reference: reference });
+            paymentsProcessed.push(payment);
+        });
+    };
+
+    var failRemittance = function (remittance) {
+        remittance.processing(true);
+        var reference = prompt("Transaction Reference #");
+        var reason = prompt("Reason for failure");
+        apiClient.Remittances.FailRemittance(remittance.id, reason, reference).then(function () {
+            remittances.remove(remittance);
+            remittance = $.extend({}, remittance, { status: 'Error', reason: reason, reference: reference });
+            remittancesProcessed.push(remittance);
+        });
+    };
+
+    var processRemittance = function (remittance) {
+        remittance.processing(true);
+        var reference = prompt("Transaction Reference #");
+        var newBalance = prompt("New Account Balance");
+        apiClient.Remittances.ProcessRemittance(remittance.id, newBalance, reference).then(function () {
+            remittances.remove(remittance);
+            remittance = $.extend({}, remittance, { status: 'Exito', reason: '', reference: reference });
+            remittancesProcessed.push(remittance);
+        });
+    };
+
+    return {
+        Date: date,
+        Payments: payments,
+        ProcessPayment: processPayment,
+        FailPayment: failPayment,
+        ProcessedPayments: paymentsProcessed,
+        Remittances: remittances,
+        ProcessRemittance: processRemittance,
+        FailRemittance: failRemittance,
+        ProcessedRemittances: remittancesProcessed
+    };
+};
---
+++ InvioBankApiClient.js
@@ -0,0 +1,62 @@
+﻿var ApiClient = function () {
+
+    var token = "52644e86-2b8f-43a7-8a42-af9065aa4014";
+    var bankId = "occidente";
+    //var baseUrl = "http://inviomobileapi.apphb.com/bank/"+bankId;
+    var baseUrl = "http://localhost:38397/bank/" + bankId;
+
+    return {
+        Payments: {
+            GetAllForDate: function (date) {
+                var start = moment(date).startOf('day').toISOString();
+                var end = moment(date).endOf('day').toISOString();
+                return $.get(baseUrl + "/payments/unpaid?start=" + start + "&end=" + end + "&token=" + token);
+            },
+            ProcessPayment: function (paymentOrderId, newAccountBalance, reference) {
+                var now = moment();
+                return $.post(baseUrl + "/payments/success", {
+                    token: token,
+                    paymentOrderId: paymentOrderId,
+                    paymentDate: now.format('YYYY-MM-DD'),
+                    accountBalance: newAccountBalance,
+                    reference: reference
+                });
+            },
+            FailPayment: function (paymentOrderId, reason, reference) {
+                var now = moment();
+                return $.post(baseUrl + "/payments/failure", {
+                    token: token,
+                    paymentOrderId: paymentOrderId,
+                    dateOfFailure: now.format('YYYY-MM-DD'),
+                    reason: reason,
+                    reference: reference
+                });
+            }
+        },
+        Remittances: {
+            GetAll: function () {
+                return $.get(baseUrl + "/remittances/pending?token=" + token);
+            },
+            ProcessRemittance: function (remittanceId, newAccountBalance, reference) {
+                var now = moment();
+                return $.post(baseUrl + "/remittances/success", {
+                    token: token,
+                    remittanceId: remittanceId,
+                    dateProcessed: now.format('YYYY-MM-DD'),
+                    accountBalance: newAccountBalance,
+                    reference: reference
+                });
+            },
+            FailRemittance: function (remittanceId, reason, reference) {
+                var now = moment();
+                return $.post(baseUrl + "/remittances/failure", {
+                    token: token,
+                    remittanceId: remittanceId,
+                    DateOfFailure: now.format('YYYY-MM-DD'),
+                    reason: reason,
+                    reference: reference
+                });
+            }
+        }
+    };
+};
